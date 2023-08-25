aa069a2956fc855502aa41e70ce25c393e918ecd
K1Pro <support@k1pro.net>
2023-08-25 18:01:26 +0200

    implementing api in front end

--- JS/trial.js
+++ JS/trial.js
@@ -19,13 +19,44 @@ async function getJSON(url, errorMsg = "Something went wrong") {
     // console.log(errorMsg);
   }
 }
+let rep = 0;
+let row;
 
 getJSON(`http://192.168.54.1:85/controller/task.php`).then((data) => {
-  // console.log(data);
+  console.log(data);
   data.data.retrieved_items.forEach((el) => {
     getJSON(`http://192.168.54.1:85/controller/task.php?directory=${el}`).then(
       (data) => {
-        console.log(data.data);
+        if (rep % 3 === 0) {
+          row = document.createElement("div");
+          row.classList.add("row");
+        }
+        column = document.createElement("div");
+        column.setAttribute(
+          "class",
+          "col-12 col-md-4 thirdPageHeight bg-light"
+        );
+        column.style.padding = "5px";
+        column.innerHTML = data.data.retrieved_items[0].slice(0, 30);
+        if (data.data.retrieved_items[0].includes("/")) {
+          console.log(
+            data.data.retrieved_items[0].slice(
+              data.data.retrieved_items[0].indexOf(data.data.parent_directory) -
+                1
+            )
+          );
+        } else {
+          console.log(
+            `/${data.data.parent_directory}/${data.data.retrieved_items[0]}`
+          );
+        }
+
+        row.appendChild(column);
+        if (rep % 3 === 0) {
+          document.getElementById(`app`).appendChild(row);
+        }
+
+        rep++;
       }
     );
   });
